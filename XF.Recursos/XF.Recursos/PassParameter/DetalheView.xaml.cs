8fdc744f344d18d9553c4481f4d22aecbd23e5af
aledebarba <ale@alemacedo.com>
2017-11-01 22:47:50 -0200

    1st commit

---
+++ XF.Recursos/XF.Recursos/PassParameter/DetalheView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Recursos.Global;
+
+namespace XF.Recursos.PassParameter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalheView : ContentPage
+    {
+        public DetalheView()
+        {
+
+            InitializeComponent();
+
+          
+
+            
+        }
+    }
+}
